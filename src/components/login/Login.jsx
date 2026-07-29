092d32f59efbde5521c23cffbe0c2d80be9e6cf6
Bojack8789 <Francoferraro18@gmail.com>
2026-07-29 17:30:54 -0300

    Agregando tunel de nuevo

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -22,7 +22,7 @@ function Login({ onLoginSuccess }) {
     showToast("Autenticando", `Intentando conectar para el usuario: ${usuario}`, "info", 3000);
 
     try {
-      const response = await fetch("https://slim-fibre-creativity-height.trycloudflare.com/api/login", {
+      const response = await fetch("https://city-thinks-moisture-trying.trycloudflare.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
